8efd736eeff7a6269af2b2eaadbabdd5166e6aa6
Dave <daveclydesdale@gmail.com>
2021-04-06 00:26:53 +0200

    heroku deployment prep2

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const storage = multer.memoryStorage();
 const fileUpload = multer({storage});
 const socketio = require('socket.io')
 const useSocket = require('./socket')
+const path = require('path');
 
 const app = express()
 const server = http.createServer(app)
@@ -44,8 +45,13 @@ app.use('/api/chat', require('./routes/chat'))
 
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, "client", "build")))
+  
+    app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
 }
+
 
 const PORT = process.env.PORT || 5000
 
