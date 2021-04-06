5bb37259d9eb9873504888594d48e11bde938059
Dave <daveclydesdale@gmail.com>
2021-04-06 12:15:47 +0200

    scripts rework

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const connectDB = require("./config/db")
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const fileUpload = multer({storage});
-const socketio = require('socket.io')
+// const socketio = require('socket.io')
 const useSocket = require('./socket')
 const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
 
-const options={
-  cors:true,
-  origins:["http://127.0.0.1:5001"],
- }
+// const options={
+//   cors:true,
+//   origins:["http://127.0.0.1:5001"],
+//  }
 
-const io = socketio(server, options)
+// const io = socketio(server, options)
 
 
 
@@ -30,11 +30,11 @@ app.options('*', cors());
 connectDB()
 
 //initiat web socket
-useSocket(io)
+// useSocket(io)
 
 // Init middleware
 app.use(express.json({ extended: false })) //body parser
-// app.use('/*', fileUpload.single('image'));
+app.use('/*', fileUpload.single('image'));
 
 // define routes
 app.use('/api/users', require('./routes/user'))
@@ -44,11 +44,14 @@ app.use('/api/posts', require('./routes/posts'))
 app.use('/api/chat', require('./routes/chat'))
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
-     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+// serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 const PORT = process.env.PORT || 5000
