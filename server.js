7aa5e1bc16a7cf09146a5c34916d174328e5659e
Dave <daveclydesdale@gmail.com>
2021-04-07 12:35:15 +0200

    heroku attempt

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require("express")
 const http = require('http')
+const path = require('path')
+
 const connectDB = require("./config/db")
+
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const fileUpload = multer({storage});
-const socketio = require('socket.io')
-const useSocket = require('./socket')
 
 const app = express()
 const server = http.createServer(app)
 
-const options={
-  cors:true,
-  origins:["http://127.0.0.1:5001"],
- }
-
-const io = socketio(server, options)
+const socketio = require('socket.io')
+const useSocket = require('./socket')
+const io = socketio(server)
 
 
+const dev = app.get('env') !== 'production'
 
 // allow cross-origin requests
 const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
-
 
 //connect db
 connectDB()
@@ -41,34 +39,18 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/profile', require('./routes/profile'))
 app.use('/api/posts', require('./routes/posts'))
 
+if(!dev){
+
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resplve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
 
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
 
-
-
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-
-// const port = process.env.PORT || 5000;
-
-// const app = express();
-
-// const server = http.createServer(app);
-
-// const io = socketIo(server);
-
-// let interval;
-
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//     clearInterval(interval);
-//   });
-// });
-
-
-// server.listen(port, () => console.log(`Listening on port ${port}`));
